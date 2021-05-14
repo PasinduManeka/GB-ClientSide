c8620bbc256a1b37b4693f3895116b6bd5c1acc2
PasinduManeka <manekaherat815@gmail.com>
2021-05-14 23:10:49 +0530

    changed

---
+++ GadgetBadget-ClientSide/WebContent/Fund.jsp
@@ -0,0 +1,62 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    
+<%@page import="com.Fund" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Fund Form</title>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="Components/main.js"></script>
+<link rel="stylesheet" href="./Views/bootstrap.min.css">
+<link rel="stylesheet" href="./Views/item.css">
+</head>
+<body>
+	<!-- start the card  -->
+	<div class="card">
+		<!-- Headline of the card -->
+		<h5 class="card-header border border-warning text-center ">
+			<strong>Add Funds</strong>
+		</h5>
+		<!-- start the card body -->
+		<div class="card-body container border border-info">
+			<!-- start the form -->
+			<form id="formFund" name=""formFund"" method="post" >
+				Cart id:
+				<input type="text" name="cartid" id="cartid" class="form-control border border-primary form-control-sm"><br>
+				Researcher Name:
+				<input type="text" name="name" id="name" class="form-control border border-primary form-control-sm"><br>
+				Invest Amount:
+				<input type="text" name="amount" id="amount" class="form-control border border-primary form-control-sm"><br>
+				Description:
+				<input type="text" name="description" id="description" class="form-control border border-primary form-control-sm"><br>
+				
+				<div id="alertSuccess" class="alert alert-success"></div>
+				<div id="alertDanger" class="alert alert-danger"></div><br>
+				
+				<input type="button" id="bntSave" name="bntSave" value="Save" class="btn btn-outline-success"><br>
+				
+				<input type="hidden" id="hidIDFundIDSave" name="hidIDItemIDSave" value="">
+			</form><br>
+			<!-- end the form -->
+			<!--  Display the HTML table -->
+			<div class="row container ">
+				<div class="col-12" id="colItem">
+					<%
+						Fund it  = new Fund();
+						out.print(it.readFunds());
+					%>
+				</div>
+			</div>
+			<!-- End display the HTML  -->
+		</div>
+		<!-- end the card body -->
+		
+	</div>
+	<!-- end the card  -->
+	
+	
+	
+</body>
+</html>
---
+++ GadgetBadget-ClientSide/src/com/FundApi.java
@@ -0,0 +1,101 @@
+package com;
+
+import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FundApi
+ */
+@WebServlet("/FundApi")
+public class FundApi extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FundApi() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		Fund itemObj = new Fund();
+		
+		String output = itemObj.inserFund(request.getParameter("cartid"), request.getParameter("name"), request.getParameter("amount"), request.getParameter("description"));
+		response.getWriter().write(output);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Fund itObjUpdate = new Fund();
+		Map paras = getParasMap(request);
+		
+		String output = itObjUpdate.updateFund(paras.get("hidIDFundIDSave").toString(), paras.get("cartid").toString(),  paras.get("name").toString(), paras.get("amount").toString(), paras.get("description").toString());
+		response.getWriter().write(output);
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Fund itObjDelete  = new Fund();
+		Map paras = getParasMap(request);
+		
+		System.out.println(paras.get("id").toString());
+		
+		String output = itObjDelete.deleteFund(paras.get("id").toString());
+		response.getWriter().write(output);
+		
+		
+		
+		
+	}
+	
+	private static Map getParasMap(HttpServletRequest request) {
+		Map<String,String> map = new HashMap<String, String>();
+		try {
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+			scanner.close();
+			
+			String[] params = queryString.split("&");
+			
+			for(String param : params) {
+				String[] p = param.split("=");
+				map.put(p[0], p[1]);
+			}
+			
+		}catch(Exception e) {
+			
+		}
+		return map;
+	}
+
+}
